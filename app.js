d3f4ebdcac612daa84ccab6746af0e4b60c6855b
joctan-maceda <123424180+joctan-maceda@users.noreply.github.com>
2023-11-25 00:04:00 -0600

    Ya funciona el registrar hilos, y sin duda el primer paso para que las demas funcionen. Falta agregar varias cosillas, como las alertas, el codigo que va a quedar feo etc. manana le seguimos ahorita ya es tarde :(

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
 // Importamos la libreria express::::::::::::::::::::::::::::::::::::::::::::::::::::::
 const express = require("express"); 
-
+const mysql = require("mysql");
 
 
 // Objetos para llamar los metodos de express::::::::::::::::::::::::::::::::::::::::::
 const app = express();
 
+// Conexion con la base de datos
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'maquiladora_ugc',
+    user: 'root',
+    password: ''
+});
 
 
 // Configuraciones
 app.set("view engine", "ejs");
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); // Es para analizar los datos que venga de un html
+
 app.get("/", function(req, res){ //cuando yo ingrese al servidor se renderiza una pantalla
-    res.render("index"); // la respuesta del servidor, es renderizar la pagina
+    res.render("Registro_Hilos"); // la respuesta del servidor, es renderizar la pagina
+});
+
+app.post("/validar", function(req, res){
+    const Hilos = req.body;
+
+    let codigo = Hilos.codigo;
+    let name = Hilos.name;
+
+
+    let buscar = "SELECT * FROM hilos WHERE codigo = "+codigo+" ";
+
+    conexion.query(buscar, function(error, row){
+        if(error){
+            throw error;
+        }else{
+            if(row.length > 0){
+                console.log("EL hilo ya existe");
+            }else{
+                let registrar = "INSERT INTO hilos (Codigo, Nombre) VALUES ('"+codigo+"', '"+name+"') ";
+
+                conexion.query(registrar, function(error){
+                    if(error){
+                        throw error;
+                    }else{
+                        console.log("Datos almacenados correctamente");
+                    }
+                });
+            }
+        }
+    });    
+    res.redirect("/");
 });
 
 // Middleware:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
